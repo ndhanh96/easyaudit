a9f6876f835c3d935a6dcad03c8e786a3c9c7a99
Nguyen Duy Hanh <ndhanh96@gmail.com>
2019-03-18 21:36:46 +0700

    update from old build

---
+++ jquery.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel( {
+        dots: false,
+        nav: false,
+        autoHeight:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    });
+});
